2b94189e19697f9debdd8fa1679d7b497798cb65
Jasper1315 <alessandrocargiolli95@gmail.com>
2023-06-08 15:33:32 +0200

    routing in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ElementoComponent } from './components/elemento/elemento.component';
+import { ListCardComponent } from './components/list-card/list-card.component';
+import { SettingsPageComponent } from './components/settings-page/settings-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*
+  { path: '', component: NavBarComponent ?ElementoComponent},
+  { path: 'nameList', component: ElementoComponent},
+  { path: 'Setting', component: SettingsPageComponent}, modifico io*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
