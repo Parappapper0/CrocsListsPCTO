3bf57747617f5e0da69e1381e46c1509a8441b2b
Jasper1315 <alessandrocargiolli95@gmail.com>
2023-06-13 15:10:44 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { ElementoComponent } from './components/elemento/elemento.component';
 import { ListCardComponent } from './components/list-card/list-card.component';
 import { SettingsPageComponent } from './components/settings-page/settings-page.component';
 import { ListSelectionPageComponent } from './components/list-selection-page/list-selection-page.component';
+import { PathLocationStrategy } from '@angular/common';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home',pathMatch: 'full'},
   { path: 'home', component: ListSelectionPageComponent},
   { path: 'name-list', component: ElementoComponent},
   { path: 'setting', component: SettingsPageComponent},
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'login-page', component: LoginPageComponent},
+
 ];
 
 @NgModule({
